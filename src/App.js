01ea1d040839744004301216af7d3825b3436b8c
Marvelous <86561662+marvcoolguy@users.noreply.github.com>
2025-08-29 00:49:18 +0100

    Did some finall touches, hopefully

--- src/App.js
+++ src/App.js
@@ -13,30 +13,46 @@ class StarWars extends React.Component {
       eye_color: null,
       hair_color: null,
       skin_color: null,
+      pounds: null,
       doneLoading: false,
     }
   }
 
   generateCharacter() {
-    console.log("Hello from marv")
-    const num = Math.ceil(Math.random() * 88)
-    const src = `https://akabab.github.io/starwars-api/api/id/${num}.json`
-    fetch(src) 
+  const num = Math.ceil(Math.random() * 88)
+  const src = `https://akabab.github.io/starwars-api/api/id/${num}.json`
+  
+  fetch(src)
     .then(res => res.json())
     .then(data => {
+      // Handle height
+      let height = "Unknown";
+      if (!isNaN(parseFloat(data.height))) {
+        height = (parseFloat(data.height) * 100).toFixed(0); // convert meters to cm
+      }
+
+      // Handle mass
+      let massKg = "Unknown";
+      let massLbs = "Unknown";
+      if (!isNaN(parseFloat(data.mass))) {
+        massKg = parseFloat(data.mass).toFixed(0);
+        massLbs = (parseFloat(data.mass) * 2.20462).toFixed(0);
+      }
+
       this.setState({
         imgSrc: data.image,
         name: data.name,
-        height: data.height,
-        mass: data.mass,
+        height: height,
+        mass: massKg,
+        pounds: massLbs,
         eye_color: data.eyeColor,
         hair_color: data.hairColor,
         skin_color: data.skinColor,
         doneLoading: true,
       })
-      
     })
-  }
+}
+
   render() {
     return(
       <div>
@@ -49,8 +65,8 @@ class StarWars extends React.Component {
           <div className='flex-container'>
             <div>
               <h3>Name: {this.state.name}</h3>
-              <h3>Height: {this.state.height}</h3>
-              <h3>Mass: {this.state.mass}</h3>
+              <h3>Height: {this.state.height !== "Unknown" ? `${this.state.height} cm` : "Unknown"}</h3>
+              <h3>Mass: {this.state.mass !== "Unknown" ? `${this.state.mass} kg (${this.state.pounds} lbs)` : "Unknown"}</h3>
               <h3>Eye Color: {this.state.eye_color}</h3>
               <h3>Hair Color: {this.state.hair_color}</h3>
               <h3>Skin Color: {this.state.skin_color}</h3>
